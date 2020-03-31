236ddd27e890e986411135f8956c1c12d2f965a5
Serhiy Prikhodko <serhii.prikhodko@gmail.com>
2020-03-31 20:46:21 +0300

    Add pods to project

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '13.0'
+use_frameworks!
+
+target 'new-york-times-app' do
+  pod 'Alamofire', '~> 5.0'
+end
---
+++ Pods/Target Support Files/Pods-new-york-times-app/Pods-new-york-times-app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_new_york_times_app : NSObject
+@end
+@implementation PodsDummy_Pods_new_york_times_app
+@end
